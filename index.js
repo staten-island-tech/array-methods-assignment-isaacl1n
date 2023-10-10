cf657b3534d54ef341984b6172b5e767c278a286
isaacl81 <isaaclin2007@gmail.com>
2023-10-10 11:29:59 -0400

    array completed

--- index.js
+++ index.js
@@ -3,32 +3,42 @@ console.log("connected")
 const countries = [
     {
         officialName: "French Republic",
-        population: "",
+        population: "68,184,000",
         capital: "Paris",
-        securityCouncil: true
+        religion: ["50% Christianity", "33% Unaffiliated", "4% Islam", "2% Bhuddism", "1% Judaism", "1% Other", "9% Unknown"],
+        federation: false,
+        securityCouncil: true,
     },
     {
         officialName: "People's Republic of China",
-        population: "",
+        population: "1,411,750,000",
         capital: "Beijing",
-        securityCouncil: true
+        religion: ["74.5% Unaffiliated/Folk Religion", "18.3% Bhuddism", "5.2% Christianity", "1.6% Islam", "0.4% Other"],
+        federation: false,
+        securityCouncil: true,
     },
     {
         officialName: "Russian Federation",
-        population: "",
+        population: "146,424,729",
         capital: "Moscow",
-        securityCouncil: true
+        religion: ["63% Christianity", "23% Unaffiliated", "8% Islam", "1% Other", "4% Unknown"],
+        federation: true,
+        securityCouncil: true,
     },
     {
         officialName: "United Kingdom of Great Britain and Northen Ireland",
-        population: "",
-        captal: "London",
-        securityCouncil: true
+        population: "67,026,292",
+        capital: "London",
+        religion: ["52% Unaffiliated", "35.6% Christianity", "6.7% Islam", "3.6% Other", "2.1% Unknown"],
+        federation: false,
+        securityCouncil: true,
     },
     {
         officialName: "United States of America",
-        population: "",
+        population: "335,485,000",
         capital: "Washington D.C.",
-        securityCouncil: true
+        religion: ["63% Christianity", "29% Unaffiliated", "1% Bhuddism", "1% Hinduism", "1% Judaism", "1% Islam", "2% Other", "2% Unknown"],
+        federation: true,
+        securityCouncil: true,
     },
 ]
