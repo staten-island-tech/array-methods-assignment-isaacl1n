d6172a79d73be4d8dae2fd27d1c5e8e83a4153d5
isaacl81 <isaaclin2007@gmail.com>
2023-10-10 09:30:24 -0400

    properties

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 console.log("connected")
 
-const arrayName = []
+const countries = [
+    {
+        officialName: "French Republic",
+        population: "",
+        capital: "Paris",
+        securityCouncil: true
+    },
+    {
+        officialName: "People's Republic of China",
+        population: "",
+        capital: "Beijing",
+        securityCouncil: true
+    },
+    {
+        officialName: "Russian Federation",
+        population: "",
+        capital: "Moscow",
+        securityCouncil: true
+    },
+    {
+        officialName: "United Kingdom of Great Britain and Northen Ireland",
+        population: "",
+        captal: "London",
+        securityCouncil: true
+    },
+    {
+        officialName: "United States of America",
+        population: "",
+        capital: "Washington D.C.",
+        securityCouncil: true
+    },
+]
